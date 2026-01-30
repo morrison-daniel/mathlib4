dfd9e633721123d6fe59833be457a807f97baad0
morrison-daniel <39346894+morrison-daniel@users.noreply.github.com>
2026-01-30 14:41:03 -0800

    improve Set.powersetCard lemmas

--- Mathlib/LinearAlgebra/ExteriorPower/Basic.lean
+++ Mathlib/LinearAlgebra/ExteriorPower/Basic.lean
@@ -63,15 +63,15 @@ def ιMulti : M [⋀^Fin n]→ₗ[R] (⋀[R]^n M) :=
 family of `n`fold exterior products of elements of `v`, seen as members of the
 `n`th exterior power. -/
 noncomputable def ιMulti_family {I : Type*} [LinearOrder I] (v : I → M)
-    (s : {s : Finset I // Finset.card s = n}) : ⋀[R]^n M :=
-  ιMulti R n fun i ↦ v <| Finset.orderIsoOfFin s.val s.property i
+    (s : Set.powersetCard I n) : ⋀[R]^n M :=
+  ιMulti R n (v ∘ (Set.powersetCard.ofFinEmbEquiv.symm s))
 
 lemma ιMulti_family_eq_coe_comp {I : Type*} [LinearOrder I] (v : I → M) :
     ExteriorAlgebra.ιMulti_family R n v = (↑) ∘ ιMulti_family R n v :=
   rfl
 
 @[simp] lemma ιMulti_family_apply_coe {I : Type*} [LinearOrder I] (v : I → M)
-    (s : {s : Finset I // Finset.card s = n}) :
+    (s : Set.powersetCard I n) :
     ιMulti_family R n v s = ExteriorAlgebra.ιMulti_family R n v s := rfl
 
 variable (M)
@@ -281,7 +281,7 @@ lemma map_comp_ιMulti_family {I : Type*} [LinearOrder I] (v : I → M) (f : M �
 
 @[simp]
 lemma map_apply_ιMulti_family {I : Type*} [LinearOrder I] (v : I → M) (f : M →ₗ[R] N)
-    (s : {s : Finset I // s.card = n}) :
+    (s : Set.powersetCard I n) :
     (map n f) (ιMulti_family R n v s) = ιMulti_family R n (f ∘ v) s := by
   simp only [ιMulti_family, map, alternatingMapLinearEquiv_apply_ιMulti]
   rfl
